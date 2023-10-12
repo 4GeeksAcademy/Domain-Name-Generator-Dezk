8807d0d4e6f729545a3aa687bae121c3cfe5f599
Rodrigo Barrera <138847032+DezkDev@users.noreply.github.com>
2023-10-12 13:06:00 +0000

    Rectificacion de generacion aleatoria por generacion de todas las posibles combinaciones

--- src/app.js
+++ src/app.js
@@ -1,24 +1,22 @@
-import "./style.css";
+/* eslint-disable */
+import "bootstrap";
+import "bootstrap";
 
 let pronoun = ["el", "un", "algun", "otro"];
 let adj = ["gran", "super", "genial"];
 let noun = ["futbolista", "boxeador", "programador", "doctor", "constructor"];
 let domain = [".com", ".net", ".org", ".info", ".es", ".cl"];
 
-function azar(longitudArray) {
-  return Math.floor(Math.random() * longitudArray);
+function print() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`);
+        }
+      }
+    }
+  }
 }
 
-function generarDominioJS() {
-  let pronounAzar = pronoun[azar(pronoun.length)];
-  let adjAzar = adj[azar(adj.length)];
-  let nounAzar = noun[azar(noun.length)];
-  let domainAzar = domain[azar(domain.length)];
-  let nombreDominio = `${pronounAzar}${adjAzar}${nounAzar}${domainAzar}`;
-
-  document.getElementById("nombreDominioHTML").innerHTML = nombreDominio;
-}
-
-document
-  .getElementById("generarDominioHTML")
-  .addEventListener("click", generarDominioJS);
+print();
