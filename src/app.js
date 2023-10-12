b85c6f1dcd29ed173bb6dfdf090919f423447f19
Rodrigo Barrera <138847032+DezkDev@users.noreply.github.com>
2023-10-12 00:40:14 +0000

    Domain Name Generator finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["el", "un", "algun", "otro"];
+let adj = ["gran", "super", "genial"];
+let noun = ["futbolista", "boxeador", "programador", "doctor", "constructor"];
+let domain = [".com", ".net", ".org", ".info", ".es", ".cl"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function azar(longitudArray) {
+  return Math.floor(Math.random() * longitudArray);
+}
+
+function generarDominioJS() {
+  let pronounAzar = pronoun[azar(pronoun.length)];
+  let adjAzar = adj[azar(adj.length)];
+  let nounAzar = noun[azar(noun.length)];
+  let domainAzar = domain[azar(domain.length)];
+  let nombreDominio = `${pronounAzar}${adjAzar}${nounAzar}${domainAzar}`;
+
+  document.getElementById("nombreDominioHTML").innerHTML = nombreDominio;
+}
+
+document
+  .getElementById("generarDominioHTML")
+  .addEventListener("click", generarDominioJS);
